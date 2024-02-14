d6a3bf0b8e892777f7b50c2b66dfd0908636a83b
Victor Julio Montero Alfaro <61398683+VictorMA345@users.noreply.github.com>
2024-02-14 11:09:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry OatMeal")
+streamlit.text("kale, spinach and Rocket Smoothie")
+streamlit.text("Hard Boiled free range Eggs")
