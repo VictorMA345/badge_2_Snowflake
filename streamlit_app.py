86fd0f7eeb5d921114a8e94fa42aa25dd26fc3bf
Victor Julio Montero Alfaro <61398683+VictorMA345@users.noreply.github.com>
2024-02-18 21:09:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice)
 
 
 my_data_row = my_cur.fetchone();
