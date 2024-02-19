ba2bff774afd2d9d1d0f7a085ca25b0aba9f5353
Victor Julio Montero Alfaro <61398683+VictorMA345@users.noreply.github.com>
2024-02-18 18:13:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 import pandas
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry OatMeal")
